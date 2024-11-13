820ead28b3300790ccf7accb01c68cf0b59465b6
Giuliao <wangguang16@outlook.com>
2024-11-13 15:14:54 +0800

    feat: add ui resizable&&form the structure of first page

--- components/dynamic-panel.tsx
+++ components/dynamic-panel.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils"
 import { Card } from '@/components/ui/card';
 
 export function DynamicPanel() {
@@ -23,13 +24,17 @@ export function DynamicPanel() {
   }, []);
 
   return (
-    <>
-      {
-        showPanel && <Card className="w-[90%] bg-white mx-10 h-80 absolute bottom-8 z-10 p-4 animate-in fade-in duration-500" >
-          which key
-        </Card>
+    <Card className={cn(
+      "w-[calc(100%-2rem)] bg-gray-200/30 backdrop-blur-sm mx-10 h-80 absolute bottom-4 z-10 p-4 animate-in fade-in duration-500",
+      showPanel ? "visible" : "hidden"
+    )}>
+      which key
+      <ul className="list-disc">
+        <li>
 
-      }
-    </>
+          show login
+        </li>
+      </ul>
+    </Card>
   );
 }
