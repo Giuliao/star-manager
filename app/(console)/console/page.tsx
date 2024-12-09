249048629d61751fba9697e7a0853f030bdda25d
Giuliao <wangguang16@outlook.com>
2024-12-09 22:28:50 +0800

    feat:init star list

--- app/(console)/console/page.tsx
+++ app/(console)/console/page.tsx
@@ -3,10 +3,18 @@ import {
   ResizablePanel,
   ResizablePanelGroup
 } from "@/components/ui/resizable";
+import { Card } from "@/components/ui/card";
 import { DynamicPanel } from "@/components/dynamic-panel";
-import { getUser } from "@/lib/actions/github";
+import { getStarList } from "@/lib/actions/github";
+import { cn } from "@/lib/utils";
 
 export default async function Home() {
+
+  const starList = (await getStarList().catch(err => {
+    console.log(err);
+    return { data: [] };
+  })).data;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <DynamicPanel />
@@ -22,14 +30,20 @@ export default async function Home() {
         <ResizablePanel defaultSize={80}>
           <ResizablePanelGroup direction="horizontal">
             <ResizablePanel defaultSize={30}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Items</span>
+              <div className="flex items-center justify-start flex-col p-2 gap-3 h-[99vh] overflow-y-auto">
+
+                {
+                  starList.map((item: any, index: number) => {
+                    return <Card className={cn("w-full h-32 p-2 hover:border-solid hover:border-l-sky-500 cursor-pointer")} key={index} >
+                      {item.full_name}
+                    </Card>
+                  })
+                }
               </div>
             </ResizablePanel>
             <ResizableHandle />
             <ResizablePanel defaultSize={70}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Content of item</span>
+              <div className="grid grid-cols-4 h-full p-4 gap-4">
               </div>
             </ResizablePanel>
           </ResizablePanelGroup>
